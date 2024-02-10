2a96b37d2fd55966c9ba89bac0ebf6feba92e394
Josh-sketch <83682736+Josh-sketch@users.noreply.github.com>
2024-02-10 14:57:37 +0100

    Update electric_safe_project.py

--- electric_safe_project.py
+++ electric_safe_project.py
@@ -1,6 +1,3 @@
-# Matrix Keypad
-# NerdCave - https://www.youtube.com/channel/UCxxs1zIA4cDEBZAHIJ80NVg - Subscribe if you found this helpful.
-# Github - https://github.com/Guitarman9119
 
 from machine import Pin
 import utime
